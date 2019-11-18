11c1a4971055262344274a3b4a1a4d4a7d6f4ca7
Nathaniel Suriawijaya <nsuriawijaya@gmail.com>
2019-11-18 12:38:45 -0800

    Fix Google API Key

--- frontend/src/components/GoogleMaps/google_maps.js
+++ frontend/src/components/GoogleMaps/google_maps.js
@@ -1,25 +1,27 @@
 import { Map, GoogleApiWrapper } from 'google-maps-react';
 import React from 'react';
+const API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
 
 const mapStyles = {
-  width: '100%',
-  height: '100%',
+  width: '500px',
+  height: '500px',
 };
 
 class GoogleMap extends React.Component{
   render() {
     return (
-      <Map
-        google={this.props.google}
-        zoom={8}
-        style={mapStyles}
-        initialCenter={{ lat: 47.444, lng: -122.176 }}
-      />
+      <div className="google-map">
+        <Map
+          google={this.props.google}
+          zoom={8}
+          style={mapStyles}
+          initialCenter={{ lat: 37.788268, lng: -122.407545}}
+        />
+      </div>
     );
   }
 }
 
 export default GoogleApiWrapper({
-  apiKey: 'AIzaSyDs6-GT7EdCy9OUh7OXZReKwJt3gafFKks'
+  apiKey: API_KEY
 })(GoogleMap);
-// export default GoogleMap;
