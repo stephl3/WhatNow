c3d62766d684f82a8b95631f7930a92fdcd83f34
lisaw97 <lisawen0614@gmail.com>
2019-11-18 10:03:17 -0800

    Frontend initial setup

---
+++ frontend/src/components/root.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import { Provider } from 'react-redux';
+// import { HashRouter } from 'react-router-dom';
+// import App from './app';
+
+const Root = ({ store }) => (
+    // <Provider store={store}>
+    //     <HashRouter>
+    //         <App />
+    //     </HashRouter>
+    // </Provider>
+    <div>
+        HI
+    </div>
+);
+
+export default Root;
