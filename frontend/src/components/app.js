f95ccafbf70efd5c83fb0bb840213ab883737aa1
lisaw97 <lisawen0614@gmail.com>
2019-11-18 11:55:22 -0800

    Uncomment container imports

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,19 +1,19 @@
 import React from "react";
 import { AuthRoute } from "../util/route_util";
 import { Switch } from "react-router-dom";
-// import NavBarContainer from "./nav/navbar_container";
+import NavBarContainer from "./nav/navbar_container";
 
 import SplashPage from "./splash/splash_page";
-// import LoginFormContainer from "./session/login_form_container";
-// import SignupFormContainer from "./session/signup_form_container";
+import LoginFormContainer from "./session/login_form_container";
+import SignupFormContainer from "./session/signup_form_container";
 
 const App = () => (
     <div>
-        {/* <NavBarContainer /> */}
+        <NavBarContainer />
         <Switch>
             <AuthRoute exact path="/" component={SplashPage} />
-            {/* <AuthRoute exact path="/login" component={LoginFormContainer} /> */}
-            {/* <AuthRoute exact path="/signup" component={SignupFormContainer} /> */}
+            <AuthRoute exact path="/login" component={LoginFormContainer} />
+            <AuthRoute exact path="/signup" component={SignupFormContainer} />
         </Switch>
     </div>
 );
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -28,7 +28,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const root = document.getElementById("root");
 
-  // ReactDOM.render(<Root store={store} />, root);
   ReactDOM.render(<Root store={store} />, root);
 
 });
