269cad6aada218fd29e0c6c539001aa96725303b
chris482 <christrinh5@gmail.com>
2019-11-18 17:43:25 -0800

    Markers rendering on google maps, no details yet

--- frontend/src/components/GoogleMaps/google_maps.jsx
+++ frontend/src/components/GoogleMaps/google_maps.jsx
@@ -1,24 +1,32 @@
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 import React from 'react';
+import { link } from 'fs';
 const API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
 
 const mapStyles = {
-  width: '500px',
-  height: '500px',
+  width: '100%',
+  height: '100%',
 };
 
 class GoogleMap extends React.Component{
   constructor(props) {
-    super(props)
+    super(props);
+    // this.createMarkers = this.createMarkers.bind(this);
   }
 
   componentDidMount() {
-    // this.props.fetchNearbyPlaces();
+    this.props.fetchNearbyPlaces();
   }
 
-  // showPlaces() {
-
-  // }
+  createMarkers() {
+    this.props.places.map(place => {
+      let lat = place.geometry.location.lat;
+      let lng = place.geometry.location.lng;
+      return (
+        <Marker position={{lat: lat, lng: lng}}/>
+      )
+    })
+  }
 
   
   render() {
@@ -31,6 +39,12 @@ class GoogleMap extends React.Component{
           initialCenter={{ lat: 37.788268, lng: -122.407545}}
         >
           <Marker position={{ lat: 37.788268, lng: -122.407545 }} />
+          {this.props.places.map(marker => (
+            <Marker
+              position={{lat:marker.geometry.location.lat,
+              lng:marker.geometry.location.lng}}
+            />
+          ))}
         </Map>
       </div>
     );
--- frontend/src/reducers/places_reducer.js
+++ frontend/src/reducers/places_reducer.js
@@ -1,7 +1,7 @@
 
 import { RECEIVE_NEARBY_PLACES } from '../actions/find_places_actions';
 
-const PlacesReducer = (oldState = {}, action) => {
+const PlacesReducer = (oldState = [], action) => {
   Object.freeze(oldState);
   switch (action.type) {
     case RECEIVE_NEARBY_PLACES:
