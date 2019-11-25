af059f8ce9d1b1a4da58a3a55675be8a5e933ecf
Nathaniel Suriawijaya <nsuriawijaya@gmail.com>
2019-11-24 18:56:23 -0800

    pre heroku

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ const passport = require("passport");
 const users = require("./routes/api/users");
 const whativities = require("./routes/api/whativities");
 
+const path = require('path');
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
+
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
