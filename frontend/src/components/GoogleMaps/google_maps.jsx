7343ab906fa874fdef4cff179b4583893db77c0e
Chris Trinh <christrinh5@gmail.com>
2019-12-03 10:48:48 -0800

    Place markers on maps

--- frontend/src/components/GoogleMaps/google_maps.jsx
+++ frontend/src/components/GoogleMaps/google_maps.jsx
@@ -13,35 +13,33 @@ class GoogleMap extends React.Component{
   }
 
   render() {
-    // let positions = [];
-    // const { spontaneous, popular, friends } = this.props;
-    
-    // if (spontaneous.length === 0) {
-    //   return null;
-    // }
-    
-    //   for (let i = 0; i < spontaneous.length; i++) {
-    //     if (spontaneous[i][1] === 'yelp') {
-    //       positions.push([spontaneous[i][0].latitude, spontaneous[i][0].longitude]);
-    //     } else {
-    //       positions.push([spontaneous[i][0].geometry.location.lat, spontaneous[i][0].geometry.location.lng])
-    //     }
-    //   }
 
     const { whativity } = this.props
-
+    debugger
     return (
       <div className="google-map" style={{ height: '200px', width: '200px' }}>
         <Map
           google={this.props.google}
           zoom={13}
           style={mapStyles}
-          initialCenter={{ lat: 37.788268, lng: -122.407545 }}
+          initialCenter={{lat: 37.788268, lng: -122.407545} }
         >
-          <Marker position={{ lat: whativity.latitude , lng: whativity.longitude }} />
-          {/* <Marker position={{ lat: positions[0][0], lng: positions[0][1] }} />
-          <Marker position={{ lat: positions[1][0], lng: positions[1][1] }} />
-          <Marker position={{ lat: positions[2][0], lng: positions[2][1] }} /> */}
+           <Marker 
+            title={'You are here!'}
+            name={'Union Square'}
+            position={{ 
+              lat: 37.788268,
+              lng: -122.407545
+            }} 
+          />
+          <Marker 
+            title={'HELLO'}
+            name={'PLACE'}
+            position={{ 
+              lat: whativity.latitude, 
+              lng: -(whativity.longitude) 
+            }} 
+          />
         </Map>
       </div>
     );
