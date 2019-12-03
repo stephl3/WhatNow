8c821431dd91b004a39bb334170d622a86bf61c5
Chris Trinh <christrinh5@gmail.com>
2019-12-03 11:14:45 -0800

    Place infoboxes on markerclick

--- frontend/src/components/GoogleMaps/google_maps.jsx
+++ frontend/src/components/GoogleMaps/google_maps.jsx
@@ -1,4 +1,4 @@
-import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
 import React from 'react';
 const API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
 
@@ -10,36 +10,69 @@ const mapStyles = {
 class GoogleMap extends React.Component{
   constructor(props) {
     super(props);
+    this.state = {
+      showingInfoWindow: false, //Hides or the shows the infoWindow
+      activeMarker: {}, //Shows the active marker upon click
+      selectedPlace: {} //Shows the infoWindow to the selected place upon a marker
+    };
+    this.onMarkerClick = this.onMarkerClick.bind(this);
+    this.onClose = this.onClose.bind(this);
   }
 
-  render() {
+  onMarkerClick = (props, marker, e) => {
+    return (
+      this.setState({
+        selectedPlace: props,
+        activeMarker: marker,
+        showingInfoWindow: true
+      })
+    )
+  }
 
-    const { whativity } = this.props
-    debugger
+  onClose = () => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  };
+
+  render() {
+    const { whativity } = this.props;
     return (
       <div className="google-map" style={{ height: '200px', width: '200px' }}>
         <Map
           google={this.props.google}
-          zoom={13}
+          zoom={11}
           style={mapStyles}
           initialCenter={{lat: 37.788268, lng: -122.407545} }
         >
            <Marker 
-            title={'You are here!'}
-            name={'Union Square'}
+            onClick={this.onMarkerClick}
+            name={'Current Location'}
             position={{ 
               lat: 37.788268,
               lng: -122.407545
             }} 
           />
           <Marker 
-            title={'HELLO'}
-            name={'PLACE'}
+            onClick={this.onMarkerClick}
+            name={whativity.name}
             position={{ 
               lat: whativity.latitude, 
               lng: -(whativity.longitude) 
             }} 
           />
+          <InfoWindow
+            marker={this.state.activeMarker}
+            visible={this.state.showingInfoWindow}
+            onClose={this.onClose}
+          >
+            <div className="map-info-window">
+              {this.state.selectedPlace.name}
+            </div>
+          </InfoWindow>
         </Map>
       </div>
     );
